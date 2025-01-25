7588ddd8033ffa34aa1c3352f87d9c7da42e9c3a
yaraPB <yarakouttane234@gmail.com>
2025-01-25 16:14:17 +0100

    Solving the issues

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+<<<<<<< HEAD
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+=======
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+>>>>>>> c80405e (Initial commit from Create Next App)
   ],
   theme: {
     extend: {
